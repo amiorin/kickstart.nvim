f80af1d7468015d2fbc6fc516ce1fb5f32efc335
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-06-21 12:53:53 +0000

    feat: add [F]ind group to which-key keymaps

--- init.lua
+++ init.lua
@@ -411,6 +411,7 @@ do
     -- Document existing key chains
     spec = {
       { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
+      { '<leader>f', group = '[F]ind', mode = { 'n', 'v' } },
       { '<leader>t', group = '[T]oggle' },
       { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
       { 'gr', group = 'LSP Actions', mode = { 'n' } },
