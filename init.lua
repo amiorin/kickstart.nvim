ed7737463fff740a7c5ed469616cc3ed7d9d7758
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-06-21 15:38:04 +0200

    fix: suppress Lua param-type-mismatch diagnostic in LSP config

--- init.lua
+++ init.lua
@@ -775,6 +775,7 @@ do
           if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
         end
 
+        ---@diagnostic disable-next-line: param-type-mismatch
         client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
           runtime = {
             version = 'LuaJIT',
