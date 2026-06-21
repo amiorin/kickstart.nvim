c9841721bdbfc488af755537400bff4dc3d5321a
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-06-21 12:35:26 +0000

    feat: add which-key buffer group and <leader>bn keymap for new buffer

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -5,5 +5,11 @@ require('snacks').setup {
   bufdelete = { enabled = true },
 }
 
+local ok, wk = pcall(require, 'which-key')
+if ok then wk.add { { '<leader>b', group = '[B]uffer' } } end
+
 -- Close the current buffer while keeping its window/split open.
 vim.keymap.set('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = 'Delete buffer' })
+
+-- Create a new empty buffer in the current window.
+vim.keymap.set('n', '<leader>bn', '<cmd>enew<cr>', { desc = 'New buffer' })
